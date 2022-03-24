d647bc69db4d9c6ea44f433cb4a848f84f37b08f
Pneumbra <102304208+Varisaar@users.noreply.github.com>
2022-03-24 12:28:28 -0400

    Potential changes for the sectionA area. Unable to test at work due to C/C++ not wanting to debug

--- LoanCalculator.c
+++ LoanCalculator.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "Banner.h"
 
-float payment, interestRate, i, currentValue, futureValue, futureValueCont;
-
+float payment, interestRate, i, currentValue, futureValueCont;
+double futureValue;
 int years;
 
 int sectionA()
@@ -10,11 +10,17 @@ int sectionA()
     double payment = 500;
     double interestRate = 0.02/12.0;
     years = 5;
+    i = 1;
+    sectionABanner();
+    while (i < years)
+    {
+        futureValue = payment * (1 + interestRate) * 5;
+        printf("future value $ %.2f", futureValue);
+    }
 }
 // TODO Use loops to generate the values of each row
 
 // TODO Use functions to calculate the future value and the future value with continuous compounding
-// sectionABanner();
 
 // Section B
 
